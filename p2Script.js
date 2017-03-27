9700147f150db497936bc8b8d7fd88e872773863
mistakuaku <dragonz139@hotmail.com>
2017-03-27 09:45:24 -0700

    Added validation that works across pages

--- p2Script.js
+++ p2Script.js
@@ -1,12 +1,3 @@
-
-//learn how to use cookies
-function timer() {
-  var time = 0;
-  time ++;
-  console.log(time);
-}
-
-
 function validation()
 {
   var answer1, answer2, answer3;
@@ -15,20 +6,38 @@ function validation()
   answer3 = "-6,-6,-12,-18,-30,-48";
 
   var page = document.title;
+  var chk;
+  var guestAnswer;
 
   if (page == "Question 1")
   {
-    sessionStorage.q1 = document.getElementById("question1").value;
-    console.log("I got to if statement 1!");
-    console.log(sessionStorage.q1);
+    guestAnswer = document.getElementById("question1").value;
+
+    chk = guestAnswer.localeCompare(answer1);
+    if (chk == 0)
+    {
+      sessionStorage.correctAnswer = 1;
+    }
   }
   else if (page == "Question 2")
   {
-    sessionStorage.q2 = document.getElementById("question2").value;
+    guestAnswer = document.getElementById("question2").value;
+
+    chk = guestAnswer.localeCompare(answer2);
+    if (chk == 0)
+    {
+      sessionStorage.correctAnswer = 2;
+    }
   }
   else if (page == "Question 3")
   {
-    sessionStorage.q3 = document.getElementById("question3").value;
+    guestAnswer = document.getElementById("question3").value;
+
+    chk = guestAnswer.localeCompare(answer3);
+    if (chk == 0)
+    {
+      sessionStorage.correctAnswer = 3;
+    }
   }
   else if (page == "Results")
   {
